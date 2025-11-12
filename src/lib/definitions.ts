39ee7656827c3b41ca22b995a6b25751804a8a58
Humberto Ribeiro <betofoxmal@gmail.com>
2025-11-11 22:29:02 -0300

    feat: add image profile auth

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -158,6 +158,7 @@ export type FormStateUserUpdate =
         errors?: {
             name?: string[];
             email?: string[];
+            image?: string[];
         };
         message?: string;
         success?: boolean;
